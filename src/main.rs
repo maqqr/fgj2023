42fbe087a36dbbcf767fff6c804a1abdd34e3401
Maqqr <maqqr@users.noreply.github.com>
2023-02-03 18:48:05 +0200

    Added 3d camera and cube

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod shaders;
+
 use bevy::prelude::*;
 
 fn main() {
@@ -11,38 +13,43 @@ fn main() {
 #[derive(Component)]
 struct Movement;
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>,) {
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        Name::new("MainCamera"),
+    ));
 
-    let mut transform = Transform::default();
-    transform.translation = Vec3::new(100.0, 100.0, 0.0);
+    let cube_material = materials.add(Color::rgb(1.0, 0.0, 0.0).into());
+    let cube_mesh = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
 
     commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(10.0, 10.0)),
-                ..Default::default()
-            },
-            transform,
+        PbrBundle {
+            mesh: cube_mesh.clone(),
+            material: cube_material.clone(),
+            transform: Transform::from_translation(Vec3::new(1.0, 1.0, 1.0)),
             ..default()
         },
         Movement,
+        Name::new("Cube"),
     ));
 }
 
 fn movement_system(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Transform, With<Movement>>) {
     for (mut transform) in query.iter_mut() {
         if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += 1.0;
+            transform.translation.y += 0.01;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= 1.0;
+            transform.translation.y -= 0.01;
         }
         if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= 1.0;
+            transform.translation.x -= 0.01;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += 1.0;
+            transform.translation.x += 0.01;
         }
 
         // wrap player around screen
