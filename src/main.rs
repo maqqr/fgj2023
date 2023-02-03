0a0dae06f1b07ee66732bd8ffbb762397f14a5fd
Maqqr <maqqr@users.noreply.github.com>
2023-02-03 17:10:38 +0200

    Code compiles now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use math::prelude::*;
 
 fn main() {
     App::new()
@@ -9,53 +8,55 @@ fn main() {
         .run();
 }
 
-enum Movement {}
+#[derive(Component)]
+struct Movement;
 
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dComponents::default());
+    commands.spawn(Camera2dBundle::default());
 
     let mut transform = Transform::default();
     transform.translation = Vec3::new(100.0, 100.0, 0.0);
 
-    commands.spawn(SpriteComponents {
-        material: Material::default().with_albedo(Color::rgb(1.0, 0.0, 0.0)),
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(10.0, 10.0)),
-            ..Default::default()
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(10.0, 10.0)),
+                ..Default::default()
+            },
+            transform,
+            ..default()
         },
-        transform,
-        movement: Movement
-        ..Default::default()
-    });
+        Movement,
+    ));
 }
 
-fn movement_system(keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&Transform, &mut Translation)>) {
-    for (_, mut translation) in &mut query.iter() {
+fn movement_system(keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&mut Transform)>) {
+    for (mut transform) in query.iter_mut() {
         if keyboard_input.pressed(KeyCode::Up) {
-            translation.y += 1.0;
+            transform.translation.y += 1.0;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            translation.y -= 1.0;
+            transform.translation.y -= 1.0;
         }
         if keyboard_input.pressed(KeyCode::Left) {
-            translation.x -= 1.0;
+            transform.translation.x -= 1.0;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            translation.x += 1.0;
+            transform.translation.x += 1.0;
         }
 
         // wrap player around screen
-        if translation.x < 0.0 {
-            translation.x = 199.0;
+        if transform.translation.x < 0.0 {
+            transform.translation.x = 199.0;
         }
-        if translation.x > 200.0 {
-            translation.x = 0.0;
+        if transform.translation.x > 200.0 {
+            transform.translation.x = 0.0;
         }
-        if translation.y < 0.0 {
-            translation.y = 199.0;
+        if transform.translation.y < 0.0 {
+            transform.translation.y = 199.0;
         }
-        if translation.y > 200.0 {
-            translation.y = 0.0;
+        if transform.translation.y > 200.0 {
+            transform.translation.y = 0.0;
         }
     }
 }
